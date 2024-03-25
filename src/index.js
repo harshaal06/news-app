3deb2a4c53cb6c1474a7274120c291abb7776387
harshaal06 <harshalaglawe06@gmail.com>
2024-03-25 15:55:35 +0530

    new app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Home from "./views/Home/Home";
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<Home/>
+);
